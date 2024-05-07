c6bba8a831482d7b971a7f099c905f0d3c443589
naufaalkh <naufaalkh@gmail.com>
2024-05-07 15:07:25 +0700

    menampilkan hasil luas segitiga

--- pointer.cpp
+++ pointer.cpp
@@ -36,4 +36,5 @@ int main(){
     cout << "Masukkan Alas: ";
     cin >> sg.alas;
     cout << "Luas Persegi Panjang: " << pp.luasPersegiPanjang(pp.panjang, pp.lebar) << endl;
+    cout << "Luas Segitiga: " << sg.luasSegitiga(sg.alas, sg.tinggi) << endl;
 }
