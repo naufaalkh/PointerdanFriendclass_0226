a9d0bcd631ff2c0c60aebbcd5fde6a68e8cd28fe
naufaalkh <naufaalkh@gmail.com>
2024-05-07 15:19:37 +0700

    membuat flow nim mahasiswa

--- pointer2.cpp
+++ pointer2.cpp
@@ -9,3 +9,17 @@ class mahasiswa{
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main(){
+    mahasiswa mhs{1}; //object mhs
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; // Pointer Reference refMhs
+    refMhs.nim = 2; // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer deference pMhs
+    pMhs->nim = 3; // Narrow Operator
+    mhs.showNim();
+    return 0;
+}
