6179836799d75dbfc277e5b1e8d244d796859f87
naufaalkh <naufaalkh@gmail.com>
2024-05-07 15:36:38 +0700

    membuat class siswa

--- friendClass1.cpp
+++ friendClass1.cpp
@@ -11,3 +11,11 @@ class orang {
         friend class siswa;    
 };
 
+class siswa {
+    private:
+        int id;
+    
+    public:
+        void setId(int pId);
+        void displayAll(orang &a);
+};
